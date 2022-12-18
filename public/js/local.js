e3901a27958af2a08d2ea0b1caebc567dc69ef90
harshanurs <singerforever06@gmail.com>
2022-12-18 19:33:17 +0530

    almost final-3 add textarea text count

--- public/js/local.js
+++ public/js/local.js
@@ -7,20 +7,7 @@ function handleClick(){
   }
 }
 
-document.getElementById("character-check").addEventListener("keydown", (e)=>{
-  let value = e.target.value.length+1;
-  if(e.key==='Backspace'){
-    if(value>1){
-      value-=2;
-    }else{
-      value=0;
-    }
-  }
-  document.getElementById("character-num").textContent = value += "/120";
-});
-
-
-document.getElementById("character-check").addEventListener("change", (e)=>{
-  let value = e.target.value.length;
-  document.getElementById("character-num").textContent = value += "/120";
+document.getElementById("character-check").addEventListener("input", (event)=>{
+  let count = event.target.value.length;
+  document.getElementById('character-num').textContent = count + "/120";
 });
